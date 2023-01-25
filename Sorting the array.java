a0d093704b294c4077465c338bc59c2396ede5c2
A Basanth Reddy <99126115+BasanthreddyA@users.noreply.github.com>
2023-01-25 23:38:27 +0530

    Add files via upload

---
+++ Sorting the array.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Integer[] s={4,2,7,8,1};
+	    Arrays.sort(s);///Array inbuilt fuction are used to sort 
+		System.out.println(Arrays.toString(s)); //printing the array by using tostring
+	}
+}
+//TC=O(LOG N);
+
+
